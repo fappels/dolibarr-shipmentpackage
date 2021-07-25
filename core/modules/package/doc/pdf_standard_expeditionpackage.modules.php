6b2e8d68423887e428e20ec5e9ee50fe40e5ee1c
Francis Appels <francis.appels@yahoo.com>
2021-07-25 17:08:34 +0200

    Add triggers, not yet used.

--- core/modules/package/doc/pdf_standard_expeditionpackage.modules.php
+++ core/modules/package/doc/pdf_standard_expeditionpackage.modules.php
@@ -916,7 +916,7 @@ class pdf_standard_expeditionpackage extends ModelePDFExpeditionPackage
 	 *  @param  int	    	$showaddress    0=no, 1=yes
 	 *  @param  Translate	$outputlangs	Object lang for output
 	 *  @param  Translate	$outputlangsbis	Object lang for output bis
-	 *  @return	void
+	 *  @return	int top_head
 	 */
 	protected function _pagehead(&$pdf, $object, $showaddress, $outputlangs, $outputlangsbis = null)
 	{
